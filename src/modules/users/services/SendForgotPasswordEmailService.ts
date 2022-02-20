de1b0af992320d9b40bfeb8967fb3d15bdf7b63c
zanniboni <viny.zanchagas@gmail.com>
2022-02-20 17:28:32 -0300

    feat: implemented send email with handlebars template

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,7 +1,9 @@
 import AppError from '@shared/http/errors/AppError';
+import path from 'path';
 import { getCustomRepository } from 'typeorm';
 import { UserRepository } from '../typeorm/repositories/Users.Repository';
 import { UserTokenRepository } from '../typeorm/repositories/UserToken.Repository';
+import EtherealMail from '@config/mail/EtherealMail';
 
 interface IRequest {
   email: string;
@@ -17,9 +19,28 @@ class SendForgotPasswordEmailService {
       throw new AppError('User not found');
     }
 
-    const token = await userTokensRepository.generate(user.id);
+    const { token } = await userTokensRepository.generate(user.id);
 
-    console.log(token);
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+    await EtherealMail.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API VENDAS] Recuperação de Senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `http://localhost:3000/reset_password?token=${token}`,
+        },
+      },
+    });
   }
 }
 
